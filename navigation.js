587c2cb268404539ad2b2ebeafdf98e2a3c79796
22840-Jarinphan <22840@chomthong.ac.th>
2026-09-04 13:24:44 +0700

    Create navigation.js

---
+++ navigation.js
@@ -0,0 +1,28 @@
+<nav class="navbar">
+
+    <a href="index.html"
+       data-page="index.html">
+        หน้าแรก
+    </a>
+
+    <a href="profile.html"
+       data-page="profile.html">
+        ประวัติส่วนตัว
+    </a>
+
+    <a href="education.html"
+       data-page="education.html">
+        การศึกษา
+    </a>
+
+    <a href="hobby.html"
+       data-page="hobby.html">
+        งานอดิเรก
+    </a>
+
+    <a href="contact.html"
+       data-page="contact.html">
+        ติดต่อ
+    </a>
+
+</nav>
